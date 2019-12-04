6307a1dd113c3e9df0bd5fe9a9c8c35dbabce4d9
John Crow <johna.crow@corteva.com>
2019-12-04 09:19:56 -0600

    Tidy.

--- rand.h
+++ rand.h
@@ -1,7 +1,7 @@
 /**
  *  @file rand.h
  *  @version 0.1.0-dev0
- *  @date Thu Nov  1 10:02:09 CDT 2018
+ *  @date Wed Dec  4 09:19:37 CST 2019
  *  @copyright %COPYRIGHT%
  *  @brief FIXME
  *  @details FIXME
